7fe68032f3841c5f3830df54da6125ee3e23afe8
User <User@User-PC>
2024-05-27 16:56:15 +0300

    Add TradingRoom class

---
+++ HomeWorkThreads/src/by/itstep/workspace/port/model/entity/TradingRoom.java
@@ -0,0 +1,49 @@
+package by.itstep.workspace.port.model.entity;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TradingRoom {
+	private static final Lock lock = new ReentrantLock();
+	private static final Condition condition = lock.newCondition();
+	
+	private volatile boolean hasOffer;
+
+	public TradingRoom() {
+		hasOffer = false;
+	}
+
+	public int putOffer(int container) throws InterruptedException {
+		lock.lock();
+        try {
+            if (hasOffer == false) {
+                hasOffer = true;
+                condition.await(25, TimeUnit.SECONDS);
+                if (hasOffer) {
+                    hasOffer = false;
+                } else {
+                    container--;
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+        return container;
+    }
+
+	public int recieveOffer(int container) {
+		 lock.lock();
+	        try {
+	            if (hasOffer == true) {
+	                hasOffer = false;
+	                condition.signal();
+	                container++;
+	            }
+	        } finally {
+	            lock.unlock();
+	        }
+	        return container;
+	    }
+}
