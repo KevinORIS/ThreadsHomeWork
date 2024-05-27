ad4578b75acb34682af8a983b25d29098a574f40
User <User@User-PC>
2024-05-27 16:58:46 +0300

    add Main class controller

---
+++ HomeWorkThreads/src/by/itstep/workspace/port/controller/Main.java
@@ -0,0 +1,25 @@
+package by.itstep.workspace.port.controller;
+
+import java.util.concurrent.Semaphore;
+
+import by.itstep.workspace.port.model.entity.Port;
+import by.itstep.workspace.port.model.logic.LoadingShipCaptain;
+import by.itstep.workspace.port.model.logic.UnloadingShipCaptain;
+
+public class Main {
+	public static void main(String[] args) {
+		int brethCount = 3;
+		Semaphore semaphore = new Semaphore(brethCount);
+		
+		Port port = new Port(semaphore);
+		
+		LoadingShipCaptain ship1 = new LoadingShipCaptain(port, 1);
+		UnloadingShipCaptain ship5 = new UnloadingShipCaptain(port, 1);
+		//LoadingShipCaptain ship2 = new LoadingShipCaptain(port, 2);
+		UnloadingShipCaptain ship6 = new UnloadingShipCaptain(port, 2);
+		LoadingShipCaptain ship3 = new LoadingShipCaptain(port, 3);
+		//UnloadingShipCaptain ship7 = new UnloadingShipCaptain(port, 3);
+		//LoadingShipCaptain ship4 = new LoadingShipCaptain(port, 4);
+		//UnloadingShipCaptain ship8 = new UnloadingShipCaptain(port, 4);	
+	}
+}
