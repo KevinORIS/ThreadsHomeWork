590cea6a5e69f5a612ac65726fcaa5549b9b5e1d
User <User@User-PC>
2024-05-29 12:17:20 +0300

    Rework UnloadingShipCaptainClass

--- HomeWorkThreads/src/by/itstep/workspace/port/model/logic/UnloadingShipCaptain.java
+++ HomeWorkThreads/src/by/itstep/workspace/port/model/logic/UnloadingShipCaptain.java
@@ -5,29 +5,62 @@ import java.util.concurrent.TimeUnit;
 import by.itstep.workspace.port.model.entity.Port;
 
 public class UnloadingShipCaptain extends ShipCaptain {
-	private int containerCount = 1;
+	private int containersCount;
+	private int containersForSale;
 	private int id;
 
 	public UnloadingShipCaptain(Port port, int id) {
 		super(port);
 		this.id = id;
+		containersCount = 1;
+		containersForSale = 1;
+	}
+
+	public UnloadingShipCaptain
+	(Port port, int id, int containersCount, int containersForSale) {
+		super(port);
+		this.id = id;
+		this.containersCount = containersCount;
+		this.containersForSale = containersForSale;
 	}
 
 	@Override
 	public void run() {
 		try {
-			System.out.printf("Разгрузочный корабль %d ждет у порта\n", id);
-			TimeUnit.SECONDS.sleep(5);
+			TimeUnit.SECONDS.sleep(3);
+			System.out.println(this + " пытается заплыть в порт");
 			port.moor();
-			System.out.printf("Разгрузочный корабль %d заплыл в порт\n", id);
-			TimeUnit.SECONDS.sleep(10);
-			containerCount = port.getRoom().putOffer(containerCount);
-			System.out.printf("Разгрузочный корабль %d уплывает с %d контейнеров\n"
-					, id, containerCount);
-			TimeUnit.SECONDS.sleep(10);
+			
+			TimeUnit.SECONDS.sleep(3);
+			System.out.println(this + " заплыл в порт");
+
+			int leftContainersCount = containersCount - containersForSale;
+			
+			int attempt = 0;
+			int maxAttempt = 5;
+			
+			while (containersCount != leftContainersCount && attempt < maxAttempt) {
+				TimeUnit.SECONDS.sleep(3);
+				System.out.println(this + " капитан зашел в комнату обмена");
+				containersCount = port.getRoom().putOffer(containersCount);
+				
+				if(containersCount != leftContainersCount) {
+					TimeUnit.SECONDS.sleep(3);
+					System.out.println(this + " обменяться не удалось, капитан на складе");
+					containersCount = 
+							port.getStorage().addContainersCount(containersForSale, containersCount);
+				}
+				attempt++;
+			}
 			port.unmoor();
-			System.out.printf("Разгрузочный корабль %d уплыл\n", id);
+			System.out.printf("%s выплыл из порта с %d контейнеров\n", this, containersCount);
+
 		} catch (InterruptedException e) {
 		}
+	}
+
+	@Override
+	public String toString() {
+		return "unloading ship " + id;
 	}
 }
